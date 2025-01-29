822e6897d208b5031ce20bf12547b28485b36db9
Zaryc0 <cotgray1997@gmail.com>
2025-01-29 19:40:04 +0000

    added test directory to physics_engine_2d for use in integraiton testing, implemented unit tests for Vector2 struct

--- Rust_Implementation/physics_engine_2d/src/lib.rs
+++ Rust_Implementation/physics_engine_2d/src/lib.rs
@@ -3,13 +3,6 @@ pub mod particle;
 pub mod shape;
 pub mod vector2;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        assert_eq!(4, 4);
-    }
-}
+
 
--- Rust_Implementation/physics_engine_2d/src/vector2.rs
+++ Rust_Implementation/physics_engine_2d/src/vector2.rs
@@ -5,7 +5,7 @@ use std::ops::Mul;
 use std::ops::Div;
 use std::ops::SubAssign;
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Vector2
 {
     x  : f32,
@@ -63,6 +63,7 @@ impl Vector2
         }
     }
 }
+
 impl Sub for Vector2
 {
     type Output = Vector2;
@@ -119,6 +120,7 @@ impl Mul<Vector2> for Vector2
         }
     }
 }
+
 impl Mul<f32> for Vector2
 {
     type Output = Vector2;
@@ -127,6 +129,7 @@ impl Mul<f32> for Vector2
         Vector2{x: self.x,y: self.y}.scale(_rhs)
     }
 }
+
 impl Mul<i32> for Vector2
 {
     type Output = Vector2;
@@ -146,5 +149,122 @@ impl Div for Vector2
             x: self.x / _rhs.x,
             y: self.y / _rhs.y
         }
+    }
+}
+
+#[cfg(test)]
+mod vector2_tests 
+{
+    use super::*;
+
+    #[test]
+    fn test_vector_new () {
+        let v_test : Vector2 = Vector2::new(2.0,3.0);
+        assert_eq!(v_test.x, 2.0);
+        assert_eq!(v_test.y, 3.0);
+    }
+    #[test]
+    fn test_vector_addition () {
+        let v1 : Vector2 = Vector2::new(2.0,3.0);
+        let v2 : Vector2 = Vector2::new(1.0,8.0);
+        let v_comp : Vector2 = Vector2::new(1.0,-5.0);
+        let v_test = v1 - v2;
+        assert_eq!(v_test, v_comp);
+    }
+    #[test]
+    fn test_vector_addition_assign() {
+        let mut v1 : Vector2 = Vector2::new(2.0,3.0);
+        let v2 : Vector2 = Vector2::new(1.0,8.0);
+        v1 += v2;
+        let v_comp : Vector2 = Vector2::new(3.0,11.0);
+        assert_eq!(v1, v_comp);
+    }
+    #[test] 
+    fn test_vector_subtraction () {
+        let v1 : Vector2 = Vector2::new(2.0,3.0);
+        let v2 : Vector2 = Vector2::new(1.0,8.0);
+        let v_comp : Vector2 = Vector2::new(1.0,-5.0);
+        let v_test = v1 - v2;
+        assert_eq!(v_test, v_comp);
+    } 
+    #[test]
+    fn test_vector_subtraction_assign() {
+        let mut v1 : Vector2 = Vector2::new(2.0,3.0);
+        let v2 : Vector2 = Vector2::new(1.0,8.0);
+        v1 -= v2;
+        let v_comp : Vector2 = Vector2::new(1.0,-5.0);
+        assert_eq!(v1, v_comp);
+    }
+    #[test]
+    fn test_vector_multiplication () {
+        let v1     : Vector2 = Vector2::new(2.0,3.0);
+        let v2     : Vector2 = Vector2::new(1.0,8.0);
+        let v_comp : Vector2 = Vector2::new(2.0,24.0);
+        let v_test : Vector2 = v1 * v2;
+        assert_eq!(v_test, v_comp);
+
+        let i      : i32 = 3;
+        let v_comp : Vector2 = Vector2::new(6.0,9.0);
+        let v_test : Vector2 = v1 * i;
+        assert_eq!(v_test, v_comp);
+
+        let f      : f32 = 3.141; 
+        let v_comp : Vector2 = Vector2::new(6.282, 9.423);
+        let v_test : Vector2 = v1 * f;
+        assert_eq!(v_test,v_comp);
+    }
+    #[test]
+    fn test_vector_division () {
+        let v1 : Vector2 = Vector2::new(7.0,3.0);
+        let v2 : Vector2 = Vector2::new(2.0,8.0);
+        let v_comp : Vector2 = Vector2::new(3.5,0.375);
+        let v_test = v1 / v2;
+        assert_eq!(v_test, v_comp);
+    }
+    #[test]
+    fn test_vector_magnitude() {
+        let v1 : Vector2 = Vector2::new(3.0,4.0);
+        let comp : f32 = 5.0;
+        let test: f32 = v1.magnitude();
+        assert_eq!(test, comp);
+    }
+    #[test]
+    fn test_vector_scale() {
+        let v1     : Vector2 = Vector2::new(2.0,3.0);
+        let scale      : f32 = 3.141; 
+        let v_comp : Vector2 = Vector2::new(6.282, 9.423);
+        let v_test : Vector2 = v1 * scale;
+        assert_eq!(v_test,v_comp);
+    }
+    #[test]
+    fn test_vector_dot_product() {
+        let v1     : Vector2 = Vector2::new(2.0,3.0);
+        let v2     : Vector2 = Vector2::new(1.0,8.0);
+        let comp : f32 = 26.0;
+        let test : f32 = v1.dot_product(v2);
+        assert_eq!(test, comp);
+    }
+    #[test]
+    fn test_vector_perpendicular() {
+        let v1     : Vector2 = Vector2::new(2.0,3.0);
+        let v_comp : Vector2 = Vector2::new(3.0,-2.0);
+        let v_test : Vector2 = v1.perpendicular();
+        assert_eq!(v_test, v_comp);
+    }
+    #[test]
+    fn test_vector_normalise() {
+        let v1     : Vector2 = Vector2::new(3.0,4.0);
+        let v_comp : Vector2 = Vector2::new(0.6,0.8);
+        let v_test : Vector2 = v1.normalise();
+        assert_eq!(v_test, v_comp);
+    }
+    #[test]
+    fn test_vector_rotate() {
+
+        let pi : f32 = 3.1416;
+        let v1 : Vector2 = Vector2::new(2.0, 2.0);
+        let v_comp :  Vector2 = Vector2 ::new(-2.0000072, 1.9999927);
+        let v_test = v1.rotate(pi/2.0);
+        assert_eq!(v_test, v_comp);
     }
 }
