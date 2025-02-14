e4f3a560bf0b8cb111f897d22eb10748f0c69b11
elliotk123 <elliotkerman@hotmail.com>
2025-02-14 22:34:52 +0000

    Can now create  a window which displays colours

--- Rust_Implementation/game_engine_2d/src/main.rs
+++ Rust_Implementation/game_engine_2d/src/main.rs
@@ -1,8 +1,11 @@
 use physics_engine_2d::vector2::Vector2;
+use graphics_engine_2d::graphics_main;
 
 fn main() {
     println!("Hello, world!");
     let v1 : Vector2 = Vector2::new(2.0,3.0);
     let v2 : Vector2 = Vector2::new(1.0,8.0);
     println!("{:?} + {:?} = {:?}",v1,v2,v1 + v2);
+    graphics_main();
+
 }
