43c399f05542babbb3240e99dd94267ad27c9f2e
Elliot Kerman <elliotkerman@hotmail.com>
2025-12-20 22:49:57 +0000

    more work on implementing Io and Physics modules

--- engine_core/src/engine_io.rs
+++ engine_core/src/engine_io.rs
@@ -1,28 +1,29 @@
 use system_interface::{init_system_interface, SystemInterface};
 use engine_common::engine_bus::{
     EngineBus,
-    CompositorToSysOutChannel
+    CompositorToSysOutChannel,
+    SysInToGameLogicChannel
 };
 use engine_common::engine_module::EngineIoModule;
 
 const WIDTH : usize = 500;
 const HEIGHT : usize = 500;
 
-struct EngineIO {
+pub struct EngineIO {
     system_interface : SystemInterface
 }
 
 impl EngineIO{
-    pub fn new(&mut self) -> &mut EngineIO{
-        self.system_interface = init_system_interface();
-        self.system_interface.graphical_interface.create_window(WIDTH as u32, HEIGHT as u32, "Physics Demo", 0);
-        self
+    pub fn new() -> EngineIO{
+        let mut system_interface: SystemInterface = init_system_interface();
+        system_interface.graphical_interface.create_window(WIDTH as u32, HEIGHT as u32, "Physics Demo", 0);
+        EngineIO{
+            system_interface
+        }
     }
 }
 
 impl EngineIoModule for EngineIO{
-
-
     fn write_output(&mut self, bus : &mut EngineBus){
         match bus.compositor_to_sysout.rx.recv().unwrap(){
             CompositorToSysOutChannel::PixelBuffer {data} => {
@@ -33,7 +34,7 @@ impl EngineIoModule for EngineIO{
     }
 
     fn read_input(&mut self, bus : &mut EngineBus){
-        let keyboard_in = bus.sysin_to_logic.tx.send(
+        bus.sysin_to_logic.tx.send(
             SysInToGameLogicChannel::KeyboardEvents{
                 events : self.system_interface.keyboard_interface.poll_events()
             }
--- engine_core/src/game_state.rs
+++ engine_core/src/game_state.rs
@@ -1,25 +1,30 @@
-use super::engine_module::{ModuleId, EngineModule};
-use super::engine_bus::{EngineBus};
-use std::collections::HashMap;
+use engine_common::{
+    engine_module::{ModuleId, EngineModule, EngineIoModule},
+    engine_bus::{EngineBus}
+};
+use game_logic::game_logic_module::GameLogicModule;
 use physics_engine_2d::physics_module::PhysicsModule;
 use graphics_compositor_2d::compositor_module::CompositorModule;
-use system_interface::{init_system_interface, SystemInterface};
+use super::engine_io::EngineIO;
 
 pub struct GameState {
     // modules: HashMap<ModuleId, Box<dyn EngineModule>>
+    engine_bus : EngineBus,
     physics_2d : PhysicsModule,
     compositor_2d : CompositorModule,
-    system_interface : SystemInterface
-
+    game_logic : GameLogicModule,
+    engine_io : EngineIO,
 }
 
 impl GameState{
-    pub fn new(engine_bus : EngineBus) -> GameState
+    pub fn new(engine_bus : EngineBus, delta_t_s : f32) -> GameState
     {
         GameState { 
-            physics_2d: PhysicsModule::new(), 
+            engine_bus,
+            physics_2d: PhysicsModule::new(delta_t_s), 
             compositor_2d: CompositorModule::new(),
-            system_interface : init_system_interface()
+            engine_io : EngineIO::new(),
+            game_logic : GameLogicModule::new()
         }
     }
 
@@ -28,11 +33,11 @@ impl GameState{
     {
         match module_id
         {
-            ModuleId::PHYS2D => {self.physics_2d.run()}
-            ModuleId::COMP2D => {self.compositor_2d.run()}
-            ModuleId::SYSIN =>  {self.system_interface.keyboard_interface.poll_events()}
-            ModuleId::SYSOUT => {self.system_interface.graphical_interface.render_to_window(pixel_data, 0, 0)}
-            ModuleId::LOGIC =>
+            ModuleId::PHYS2D => {self.physics_2d.run(&mut self.engine_bus)}
+            ModuleId::COMP2D => {self.compositor_2d.run(&mut self.engine_bus)}
+            ModuleId::SYSIN =>  {self.engine_io.read_input(&mut self.engine_bus)}
+            ModuleId::SYSOUT => {self.engine_io.write_output(&mut self.engine_bus)}
+            ModuleId::LOGIC =>  {self.game_logic.run(&mut self.engine_bus)}
         }
     }
 }
